a173f81d1e726fb585735420f7039a38e47e78a8
Roberto Nogueira <enogrob@gmail.com>
2018-05-09 19:58:43 -0300

    params-status

--- servy/handler.rb
+++ servy/handler.rb
@@ -8,7 +8,7 @@ module ServyHandler
   end
 
   def self.log(conv)
-    conv.inspect
+    puts conv.inspect
     conv
   end
 
@@ -19,26 +19,48 @@ module ServyHandler
     .first\
     .split(" ")
 
-    {method: method, path: path, resp_body: ""}
+    {method: method, path: path, resp_body: "", status: nil}
   end
 
   def self.route(conv)
     if conv[:method] == "GET" && conv[:path] == "/wildthings"
       conv[:resp_body] = "Bears, Lions, Tigers"
+      conv[:status] = 200
     elsif conv[:method] == "GET" && conv[:path] == "/bears"
       conv[:resp_body] = "Teddy, Smokey, Paddington"
+      conv[:status] = 200
+    elsif conv[:method] == "GET" && conv[:path] =~ /\/bears\/(\d)/
+      conv[:resp_body] = "Bear #{$1}"
+      conv[:status] = 200
+    elsif conv[:method] == "DELETE" && conv[:path] =~ /\/bears\/(\d)/
+      conv[:resp_body] = "Deleting a bear is forbidden!"
+      conv[:status] = 403
+    else
+      conv[:resp_body] = "No #{conv[:path]} here!"
+      conv[:status] = 404
     end
     conv
   end
 
   def self.format_response(conv)
     <<~"END"
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv[:status]} #{status_reason(conv[:status])}
     Content-Type: text/html
     Content-Length: #{conv[:resp_body].length}
 
     #{conv[:resp_body]}
     END
+  end
+
+  def self.status_reason(code)
+    {
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 
 end
--- spec/servy_spec.rb
+++ spec/servy_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe 'Servy App' do
 
     it 'Responds to proper methods' do
         expect(subject).to respond_to(:handle)
+        expect(subject).to respond_to(:log)
         expect(subject).to respond_to(:parse)
         expect(subject).to respond_to(:route)
         expect(subject).to respond_to(:format_response)
+        expect(subject).to respond_to(:status_reason)
     end
 
     it 'Responds to handle properly' do
@@ -34,6 +36,15 @@ RSpec.describe 'Servy App' do
       ).to_stdout
     end
 
+    it 'Responds to log properly' do
+      conv = { method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+      expect {subject.log(conv)}.to output(
+        <<~MESSAGE
+        {:method=>\"GET\", :path=>\"/wildthings\", :resp_body=>\"Bears, Lions, Tigers\"}
+        MESSAGE
+      ).to_stdout
+    end
+
     it 'Responds to parse properly' do
       request = <<~"REQUEST"
       GET /wildthings HTTP/1.1
@@ -44,22 +55,38 @@ RSpec.describe 'Servy App' do
       REQUEST
       conv = subject.parse(request)
       expect(conv).to be_instance_of(Hash)
-      expect(conv).to match({ method: "GET", path: "/wildthings", resp_body: "" })
+      expect(conv).to match({ method: "GET", path: "/wildthings", resp_body: "", status: nil})
     end
 
     it 'Responds to route properly' do
-      conv = { method: "GET", path: "/wildthings", resp_body: "" }
+      conv = { method: "GET", path: "/wildthings", resp_body: "", status: nil}
       conv = subject.route(conv)
       expect(conv).to be_instance_of(Hash)
-      expect(conv).to match({ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" })
-      conv = { method: "GET", path: "/bears", resp_body: "" }
+      expect(conv).to match({ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers", status: 200 })
+
+      conv = { method: "GET", path: "/bears", resp_body: "", status: nil }
       conv = subject.route(conv)
       expect(conv).to be_instance_of(Hash)
-      expect(conv).to match({ method: "GET", path: "/bears", resp_body: "Teddy, Smokey, Paddington" })
+      expect(conv).to match({ method: "GET", path: "/bears", resp_body: "Teddy, Smokey, Paddington", status: 200 })
+
+      conv = { method: "GET", path: "/bears/1", resp_body: "", status: nil }
+      conv = subject.route(conv)
+      expect(conv).to be_instance_of(Hash)
+      expect(conv).to match({ method: "GET", path: "/bears/1", resp_body: "Bear 1", status: 200 })
+
+      conv = { method: "DELETE", path: "/bears/1", resp_body: "", status: nil }
+      conv = subject.route(conv)
+      expect(conv).to be_instance_of(Hash)
+      expect(conv).to match({ method: "DELETE", path: "/bears/1", resp_body: "Deleting a bear is forbidden!", status: 403 })
+
+      conv = { method: "GET", path: "/teddy", resp_body: "", status: nil }
+      conv = subject.route(conv)
+      expect(conv).to be_instance_of(Hash)
+      expect(conv).to match({ method: "GET", path: "/teddy", resp_body: "No /teddy here!", status: 404 })
     end
 
     it 'Responds to format_response properly' do
-      conv = { method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+      conv = { method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers", status: 200}
       expect {puts subject.format_response(conv)}.to output(
         <<~MESSAGE
         HTTP/1.1 200 OK
